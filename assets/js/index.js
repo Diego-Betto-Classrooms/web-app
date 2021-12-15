f3d8335fa3cf85fa2503300f0fc2525ee4f9a8fa
Diego Betto <diego@papion.it>
2021-12-15 17:06:29 +0100

    da vanilla js a jquery

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,8 +3,10 @@ import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 function login(event) {
   event.preventDefault();
 
-  const email = document.getElementById('floatingInput').value
-  const password = document.getElementById('floatingPassword').value
+  // const email = document.getElementById('floatingInput').value
+  const email = jQuery('#floatingInput').val()
+  // const password = document.getElementById('floatingPassword').value
+  const password = jQuery('#floatingPassword').val() 
 
   fetch('http://localhost:5000/api/login', {
     method: 'POST', 
@@ -23,10 +25,17 @@ function login(event) {
   });
 }
 
-const loginForm = document.getElementById('login-form');
+/*const loginForm = document.getElementById('login-form');
 if(loginForm){
   loginForm.addEventListener('submit', login)
-}
+}*/
+/*
+const loginForm = jQuery('#login-form');
+if(loginForm) {
+  loginForm.on('submit', login)
+}*/
+
+jQuery('#login-form').on('submit', login)
 
 /** NEWS */
 function renderNews() {
@@ -38,41 +47,59 @@ function renderNews() {
   }).then(function(data){
     return data.json()
   }).then(function(data){
-    const newsContainer = document.getElementById('news-container')
+    // const newsContainer = document.getElementById('news-container')
+    const newsContainer = jQuery('#news-container');
+    
     if(newsContainer && data) {
       const htmlNews = [];
-      newsContainer.innerHTML = '';
+      // newsContainer.innerHTML = '';
+      newsContainer.html('');
 
       data.forEach(function(singleNews){
-        const newsCol = document.createElement('div');
-        newsCol.className = 'col';
+        // const newsCol = document.createElement('div');
+        const newsCol = jQuery('<div />')
+        // newsCol.className = 'col';
+        newsCol.addClass('col')
 
-        const newsCard = document.createElement('div');
-        newsCard.className = 'card shadow-sm';
+        // const newsCard = document.createElement('div');
+        const newsCard = jQuery('<div />');
+        // newsCard.className = 'card shadow-sm';
+        newsCard.addClass('card shadow-sm');
 
-        const cardBody = document.createElement('div');
-        cardBody.className = 'card-body';
+        // const cardBody = document.createElement('div');
+        const cardBody = jQuery('<div />');
+        // cardBody.className = 'card-body';
+        cardBody.addClass('card-body');
 
-        const img = document.createElement('img');
-        img.className = "bd-placeholder-img card-img-top img-fluid";
-        img.width = "100%";
-        img.height = "225";
-        img.src = singleNews.image;
-        img.loading = 'lazy'
-
-        const title = document.createElement('h5');
-        title.className = 'card-title';
-        title.textContent = singleNews.title;
+        // const img = document.createElement('img');
+        const img = jQuery('<img />');
+        // img.className = "bd-placeholder-img card-img-top img-fluid";
+        img.addClass("bd-placeholder-img card-img-top img-fluid");
         
-        const text = document.createElement('p');
-        text.className = 'card-text';
-        text.textContent = singleNews.text;
+        img.attr('width', "100%");
+        img.attr('height', "225");
+        img.attr('src', singleNews.image);
+        img.attr('loading', 'lazy');
+
+        // const title = document.createElement('h5');
+        const title = jQuery('<h5 />');
+        // title.className = 'card-title';
+        title.addClass('card-title');
+        // title.textContent = singleNews.title;
+        title.text(singleNews.title);
+        
+        // const text = document.createElement('p');
+        const text = jQuery('<p />');
+        // text.className = 'card-text';
+        text.addClass('card-text');
+        // text.textContent = singleNews.text;
+        text.text(singleNews.text);
 
         cardBody.append(title, text);
         newsCard.append(img, cardBody);
-        newsCol.appendChild(newsCard);
+        newsCol.append(newsCard);
 
-        newsContainer.appendChild(newsCol) 
+        newsContainer.append(newsCol) 
       })
     }
   })
